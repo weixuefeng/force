a345f866f6035fd4276a91b7dbff56e82a9ea742
yheng2 <hengyiwei@diynova.com>
2022-06-08 21:25:34 +0800

    Update code

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -33,13 +33,12 @@ class HomeView extends GetView<HomeController> {
                     enlargeCenterPage: true,
                     aspectRatio: 2.0,
                     enableInfiniteScroll: false,
+                    viewportFraction: 0.85,
                     onPageChanged: (index, reason) {
                       controller
                           .setCurrentWalletId(controller.storedInfos[index].id);
                     }),
-                items: controller.storedInfos.map((info) {
-                  return _buildPageItem(info);
-                }).toList(),
+                items: getList(),
               ),
             ),
             Container(
@@ -55,15 +54,36 @@ class HomeView extends GetView<HomeController> {
                             .map((element) => _buildWalletItem(element))
                             .toList(),
                   )),
-            )
+            ),
           ],
+        ));
+  }
+
+  Widget button(int exampleNumber, String title) {
+    return Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+        child: Button(
+          key: ValueKey('button$exampleNumber'),
+          isSelected: controller.selectedChain == exampleNumber,
+          exampleNumber: exampleNumber,
+          title: title,
+          onPressed: () {
+            controller.selectedChain(exampleNumber);
+          },
         ));
   }
 
   Widget _buildPageItem(StoredKeyInfo info) {
     final id = info.id.toString();
+    final list = [
+      {'asset': 'images/all.png'},
+      {'asset': 'images/new.png'},
+      {'asset': 'images/eth.png'}
+    ];
+
     return Card(
-      color: Colors.blueAccent,
+      margin: const EdgeInsets.all(0),
+      color: Colors.white,
       elevation: 2.0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(20.0)),
@@ -71,15 +91,80 @@ class HomeView extends GetView<HomeController> {
       clipBehavior: Clip.antiAlias,
       semanticContainer: false,
       child: Container(
-        width: 400,
-        height: 100,
-        child: Center(
-          child: Text(
-            'Wallet $id',
-            style: TextStyle(fontSize: 20.0),
+          margin: const EdgeInsets.all(0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/card.jpeg'),
+              fit: BoxFit.fill,
+            ),
           ),
-        ),
-      ),
+          height: 100,
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 16, top: 16),
+                  child: Text(
+                    '账户 $id',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 16, top: 8),
+                  child: Text(
+                    '\$0.00',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 16, top: 16),
+                  child: Text(
+                    '账户 $id',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              Container(
+                height: 40,
+                margin: const EdgeInsets.only(left: 16, right: 16, top: 16),
+                width: double.infinity,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for (int i = 0; i < list.length; i++)
+                        Obx(() => Container(
+                              width: 48,
+                              padding:
+                                  const EdgeInsets.only(left: 4.0, right: 4.0),
+                              child: button(i + 1, list[i]['asset'] ?? ""),
+                            )),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 
@@ -94,6 +179,55 @@ class HomeView extends GetView<HomeController> {
               child: Text("Open"))
         ],
       ),
+    );
+  }
+
+  List<Widget> getList() {
+
+    return controller.storedInfos.map((info, {index}) {
+                  return _buildPageItem(info);
+                }).toList();
+  }
+}
+
+class Button extends StatelessWidget {
+  final bool isSelected;
+  final int exampleNumber;
+  final VoidCallback onPressed;
+  final String title;
+
+  const Button(
+      {required Key key,
+      required this.isSelected,
+      required this.exampleNumber,
+      required this.onPressed,
+      required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        ),
+        backgroundColor: MaterialStateProperty.all(
+            isSelected ? Color.fromRGBO(255, 255, 255, 0.8) : Color.fromRGBO(255, 255, 255, 0.2)),
+      ),
+      child: Image.asset(
+        title,
+        fit: BoxFit.fill,
+      ),
+
+      onPressed: () {
+        Scrollable.ensureVisible(
+          context,
+          duration: const Duration(milliseconds: 350),
+          curve: Curves.easeOut,
+          alignment: 0.5,
+        );
+        onPressed();
+      },
     );
   }
 }
