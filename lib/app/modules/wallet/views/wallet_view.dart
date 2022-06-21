b8a7d2c9f0f308488266bdbf5bfac700ebee1b23
weixuefeng <weixuefeng@diynova.com>
2022-06-21 15:41:43 +0800

    feat: add convert address

--- lib/app/modules/wallet/views/wallet_view.dart
+++ lib/app/modules/wallet/views/wallet_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:forcewallet/app/utils/extension.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../controllers/wallet_controller.dart';
 
@@ -17,7 +18,7 @@ class WalletView extends GetView<WalletController> {
       ),
       body: Column(children: [
         Obx(() => Text(
-              'Symbol: ${controller.mStoredWalletInfo.value.coinType.toCoinSymbol()}\n\naddress:${controller.mStoredWalletInfo.value.showAddress}',
+              'Symbol: ${controller.mStoredWalletInfo.value.coinType.toCoinSymbol()}\n\naddress:${controller.mStoredWalletInfo.value.showAddress?.toNEWAddress(1012)}',
               style: TextStyle(fontSize: 20),
             )),
         Obx(() => Text(
@@ -28,7 +29,45 @@ class WalletView extends GetView<WalletController> {
         ElevatedButton(
             onPressed: () => {controller.openSend()},
             child: Text("send transaction")),
-        ElevatedButton(onPressed: () => {}, child: Text("receive address")),
+        QrImage(
+          data:
+              '${controller.mStoredWalletInfo.value.showAddress?.toNEWAddress(1007)}',
+          version: QrVersions.auto,
+          size: 320,
+          gapless: false,
+          errorStateBuilder: (cxt, err) {
+            return Container(
+              child: Center(
+                child: Text(
+                  "Uh oh! Something went wrong...",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          },
+        ),
+        ElevatedButton(
+            onPressed: () => {
+                  Get.defaultDialog(
+                      title: '提示',
+                      content: QrImage(
+                        data: 'This QR code will show the error state instead',
+                        version: 1,
+                        size: 320,
+                        gapless: false,
+                        errorStateBuilder: (cxt, err) {
+                          return Container(
+                            child: Center(
+                              child: Text(
+                                "Uh oh! Something went wrong...",
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          );
+                        },
+                      ))
+                },
+            child: Text("receive address")),
       ]),
     );
   }
